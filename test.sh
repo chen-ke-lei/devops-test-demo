8eacb23184f7e2ab0af3fb5e8bd6c295a6531dab
chenkelei <469718402@qq.com>
2023-07-24 17:27:58 +0800

    1123123

--- test.sh
+++ test.sh
@@ -1,8 +1,7 @@
 # hack操作
 echo '{
   "registry-mirrors":["https://docker.mirrors.ustc.edu.cn"],
-  "exec-opts": ["native.cgroupdriver=systemd"],
-  "insecure-registries" : ["172.16.17.3:5000","172.16.17.2:8088","172.16.17.4:18088"]
+  "insecure-registries" : ["172.16.17.4:18088"]
    }'> /etc/docker/daemon.json
 
 systemctl daemon-reload
